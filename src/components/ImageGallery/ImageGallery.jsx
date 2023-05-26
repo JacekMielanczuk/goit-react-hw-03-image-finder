d312afd8d0ae4bca7e3d5ec88278a663c0ee79b5
JacekMielanczuk <mielanczuk1988@gmail.com>
2023-05-26 10:08:31 +0200

    jsx ImageGalleryItem
    jsx ImageGalleryItem

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import styles from './ImageGallery.module.css';
+import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,18 @@
+import react, { Component } from react;
+import styles from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
+
+export default class ImageGalleryItem extends Component {
+    render() {
+        return (
+            <li className={styles.galleryItem}>
+                <img src={this.props.smallImgURL } alt='{this.props.id}' />
+            </li>
+        )
+    }
+}
+
+ImageGalleryItem.propTypes = {
+    id: PropTypes.number.isRequired,
+    smallImgURL: PropTypes.string.isRequired,
+}
