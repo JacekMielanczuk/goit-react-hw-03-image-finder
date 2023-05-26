246feea3a546c2f9a2290afb67ad4b25c518c017
JacekMielanczuk <mielanczuk1988@gmail.com>
2023-05-26 10:36:49 +0200

    API
    API

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,1 @@
-import styles from './button.module.css';
-import PropTypes from 'prop-types';
 
-export default function Button({ onClick }) {
-  return (
-    <button type="button" onClick={onClick} className={styles.button}>
-      Load more
-    </button>
-  );
-}
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-};
